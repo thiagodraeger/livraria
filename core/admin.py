ff0fff73ba90fb03f471d69f5c9b573fa500b364
Thiago Draeger <thiagodraeger123@gmail.com>
2022-08-29 14:16:53 -0300

    aula 5

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,9 @@
+from re import A
 from django.contrib import admin
 
-from core.models import Categoria, Editora
+from core.models import Categoria, Editora, Autor, Livro
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Livro)
+admin.site.register(Autor)
